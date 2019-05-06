bf91464d945f7fdf9a23fa546b2542ca2b34a5f8
Jan Kaiser <jan.kaiser.email@googlemail.com>
2019-05-06 23:36:33 +0200

    Added loading of file information

--- source/t64.c
+++ source/t64.c
@@ -11,11 +11,26 @@ T64Tape* loadT64Tape(char* path) {
     if (buffer == NULL) return NULL;
 
     T64Tape* t = malloc(sizeof(T64Tape));
+
+    // Header
     memcpy(t->signature, buffer, 32);
-    t->version = (int) ((buffer[0x21] << 8) | buffer[0x20]);
-    t->maxEntries = (int) ((buffer[0x23] << 8) | buffer[0x22]);
-    t->usedEntries = (int) ((buffer[0x25] << 8) | buffer[0x24]);
+    t->version = (int) ((buffer[0x21]<<8) | buffer[0x20]);
+    t->maxEntries = (int) ((buffer[0x23]<<8) | buffer[0x22]);
+    t->usedEntries = (int) ((buffer[0x25]<<8) | buffer[0x24]);
     memcpy(t->name, &buffer[0x28], 24);
+
+    // Files
+    t->entries = malloc(sizeof(T64Entry) * t->usedEntries);
+    for (int i = 0; i < t->maxEntries; i++) {
+        uint8_t* raw = &buffer[0x40 + 0x10 * i];
+        t->entries[i].filetypec64s = raw[0];
+        t->entries[i].filetype1541 = raw[1];
+        t->entries[i].load = (raw[3]<<8) | raw[2];
+        t->entries[i].end = (raw[5]<<8) | raw[4];
+        t->entries[i].offset = (raw[0x0b]<<24) | (raw[0x0a]<<16) | (raw[9]<<8) | raw[8];
+        memcpy(t->entries[i].name, &raw[0x10], 16);
+        t->entries[i].data = NULL;
+    }
 
     free(buffer);
 
--- source/t64.h
+++ source/t64.h
@@ -2,11 +2,22 @@
 #define T64_H
 
 typedef struct {
+    uint8_t filetypec64s;
+    uint8_t filetype1541;
+    uint16_t load;
+    uint16_t end;
+    uint32_t offset;
+    char name[16];
+    uint8_t* data;
+} T64Entry;
+
+typedef struct {
     char signature[32];
     int version;
+    char name[24];
     int maxEntries;
     int usedEntries;
-    char name[24];
+    T64Entry* entries;
 } T64Tape;
 
 T64Tape* loadT64Tape(char* path);
