f247f10b13c241001ed279f811d42390ad84d8e0
Jan Kaiser <jan.kaiser.email@googlemail.com>
2019-05-07 14:53:46 +0200

    Finished 6502 status register implementation

--- source/emu6502.h
+++ source/emu6502.h
@@ -4,16 +4,22 @@
 #include <stdint.h>
 
 typedef struct {
-    uint8_t bla;
-} ConditionCodes6502;
+    uint8_t c:1;
+    uint8_t z:1;
+    uint8_t i:1;
+    uint8_t d:1;
+    uint8_t b:1;
+    uint8_t v:1;
+    uint8_t n:1;
+} StatusRegister6502;
 
 typedef struct {
     uint8_t a;
     uint8_t x;
     uint8_t y;
     uint8_t* memory;
-    uint8_t pc;
-    ConditionCodes6502 cc;
+    uint16_t pc;
+    StatusRegister6502 p;
 } State6502;
 
 void emulate6502(State6502* state);
