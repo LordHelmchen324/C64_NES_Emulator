934112cad496f11fca33fa274d3b9d91c637ae0b
Jan Kaiser <jan.kaiser.email@googlemail.com>
2019-05-09 13:26:33 +0200

    Defined full iNES structure

--- source/ines.h
+++ source/ines.h
@@ -4,10 +4,22 @@
 #include<stdint.h>
 
 typedef struct {
-    int prgsize;
+    char signature[4];
+    uint8_t prgsize;
+    uint8_t chrsize;
+    uint8_t mirroring:1;
+    uint8_t hasprgram:1;
+    uint8_t hastrainer:1;
+    uint8_t ignoremirroring:1;
+    uint8_t mapper;
+    uint8_t isvsunisystem:1;
+    uint8_t haspc10:1;
+    uint8_t isnes2:1;
+    uint8_t* trainer;
     uint8_t* prg;
-    int chrsize;
     uint8_t* chr;
-} INESCartridge;
+    uint8_t* pcir;
+    uint8_t* pcpr;
+} iNES;
 
 #endif
